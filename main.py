bfe8c8af7fdcea97eaf2bfb9b162fc870b54e44b
lipeiyang <peiyangli98@gmail.com>
2022-08-26 10:28:20 +0800

    fix today datetime at 00:00

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ import os
 import random
 import re
 
-today = datetime.now() + timedelta(hours=8)
+today = datetime.utcnow() + timedelta(hours=8)  # 东八区
+today = datetime.strptime(str(today.date()), "%Y-%m-%d")
 start_date = os.getenv('START_DATE')
 city = os.getenv('CITY')
 birthday = os.getenv('BIRTHDAY')
